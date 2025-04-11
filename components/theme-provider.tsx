4a2c3d090e11c8cd9dfb42916f20807937c9363e
Alex R <alexromano10321@gmail.com>
2025-04-10 19:33:32 -0700

    derr

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,13 +5,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false)
-
-  // Ensure we only render theme switching in the client to avoid hydration mismatch
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
   // Force dark theme initially to avoid flash
   React.useEffect(() => {
     document.documentElement.classList.add("dark")
