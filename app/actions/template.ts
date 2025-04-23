d341aff3fcbc081ff4a4c67519abb9ec64ede615
Alex R <alexromano10321@gmail.com>
2025-04-22 19:30:55 -0700

    der

--- app/actions/template.ts
+++ app/actions/template.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
-import prisma from "@/lib/prisma"
+import { prisma } from "@/lib/prisma"
 import { canOverrideRnRDecisions } from "@/lib/roles"
 
 interface QuestionData {
