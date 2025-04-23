69c19c15b2dd91d17fa9f3a35fd9e942236813ea
Alex R <alexromano10321@gmail.com>
2025-04-22 17:39:05 -0700

    derr

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,3 +13,5 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export default prisma
