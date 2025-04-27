96cef7c14e8924282f85e115355fb1efa497c54a
Alex R <alexromano10321@gmail.com>
2025-04-26 18:20:29 -0700

    der

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,12 @@ import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
+  // Use Accelerate only in production
+  if (process.env.NODE_ENV === 'production') {
+    return new PrismaClient().$extends(withAccelerate())
+  }
+  // Use direct connection in development
+  return new PrismaClient()
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
